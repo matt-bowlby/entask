9bb828d4368049abd22ce0afd0ba2b478d7c3291
Matt Bowlby <mbowlby794@gmail.com>
2025-02-15 22:03:26 -0800

    Add small UI demo

--- src/App.tsx
+++ src/App.tsx
@@ -1,9 +1,28 @@
 import "./App.css";
+import React, { useRef } from "react";
+import {Event, Task} from "@classes/thing/Thing";
+import Calendar from "@classes/calendar/Calendar";
 
 function App() {
+  const calendar = useRef(new Calendar("My Calendar"));
+  const [thingList, setThingList] = React.useState(calendar.current.getActiveThings());
+
+  const handleAddEvent = () => {
+    calendar.current.addEvent(new Event("My Event", calendar.current));
+    setThingList(calendar.current.getActiveThings());
+  };
+
+  const handleAddTask = () => {
+    const task = new Task("My Task", calendar.current);
+    calendar.current.addTask(task);
+    setThingList(calendar.current.getActiveThings());
+  };
+
   return (
     <>
-      <h1>Hello world! We enter in App.tsx for React</h1>
+      <button className="regular_button" onClick={handleAddEvent}>Add Event</button>
+      <button className="regular_button" onClick={handleAddTask}>Add Task</button>
+      <p>{thingList.length()} items active</p>
     </>
   );
 }
