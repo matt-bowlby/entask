cfce03dc8674dc38bc96de93955608495c8f1571
JD Giovannetti <jonathangiovannetti2@gmail.com>
2025-02-21 17:08:27 -0800

    Complete prototype for hour lines in calendar view

--- src/components/CalendarView/CalendarHours.tsx
+++ src/components/CalendarView/CalendarHours.tsx
@@ -1,10 +1,35 @@
 export default function CalendarHours() {
     return (
-        <div id="hours" className="grid grid-cols-7 overflow-y-scroll h-[80vh]">
-            {Array.from({ length: 7 }, (_, k) => (
-                <div className="bg-red-300 border-r-2 h-[600vh]">HI!</div>
-            ))}
-            <h1>the end</h1>
-        </div>
+        <>
+            {/* NOTE: this div NEEDS a specified height in order for overflow-y-scroll to work  */}
+            <div
+                id="hours"
+                className="grid grid-cols-7 overflow-y-scroll h-[80vh] relative"
+            >
+                {/* The hour lines behind the events */}
+                <div className="absolute h-[600vh] w-full">
+                    {Array.from({ length: 24 }, (_, k) => (
+                        <div
+                            key={k}
+                            className="absolute flex ml-4 left-0 right-0 items-center text-amber-600 gap-x-3"
+                            style={{ top: `${25 * k}vh` }}
+                        >
+                            {/* There might be issues here of the contents being so tall that it actually misaligns the events in the calendar */}
+                            <span>{k + 1}</span>
+                            <div className="h-[2px] w-full bg-amber-600"></div>
+                        </div>
+                    ))}
+                </div>
+
+                {/* The columns that hold the events */}
+                {Array.from({ length: 7 }, (_, k) => (
+                    <div
+                        key={k}
+                        id={`items-col-${k}`}
+                        className="border-r-2 h-[600vh] z-10"
+                    ></div>
+                ))}
+            </div>
+        </>
     );
 }
--- src/components/CalendarView/CalendarView.tsx
+++ src/components/CalendarView/CalendarView.tsx
@@ -1,6 +1,6 @@
 import CalendarDays from "@/components/CalendarView/CalendarDays/CalendarDays";
 import CalendarHeader from "@components/CalendarView/CalendarHeader";
-import CalendarHours from "@components/CalendarView/CalendarHours";
+import CalendarHours from "@/components/CalendarView/CalendarHours";
 
 export default function CalendarView() {
     return (
