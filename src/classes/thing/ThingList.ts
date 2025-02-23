e9b042c320f479994247d9fbc87847a1e5c35966
Matt Bowlby <mbowlby794@gmail.com>
2025-02-23 14:41:40 -0800

    Add getTasks(), getEvents(), and getThingById() functions

--- src/classes/thing/ThingList.ts
+++ src/classes/thing/ThingList.ts
@@ -1,4 +1,4 @@
-import Thing from "@classes/thing/Thing";
+import Thing, { Task, Event } from "@classes/thing/Thing";
 
 class ThingList {
     /**
@@ -6,12 +6,16 @@ class ThingList {
      */
     public things: Array<Thing> = [];
 
+    constructor(things: Array<Thing> = []) {
+        this.things = things;
+    }
+
     /**
      * Adds a Thing to the list.
      * @param thing - The Thing to add.
      */
-    public addThing(thing: Thing): void {
-        this.things.push(thing);
+    public addThing(...things: Array<Thing>): void {
+        this.things.push(...things);
     }
 
     /**
@@ -26,7 +30,7 @@ class ThingList {
      * Returns a copy of the list.
      * @returns A copy of the list.
      */
-    duplicate(): ThingList {
+    public duplicate(): ThingList {
         const copy = new ThingList();
         copy.things = [...this.things];
         return copy;
@@ -36,8 +40,33 @@ class ThingList {
      * Returns the length of the list.
      * @returns The length of the list.
      */
-    length(): number {
+    public length(): number {
         return this.things.length;
+    }
+
+    /**
+     * Returns the list of Tasks.
+     * @returns The list of Tasks.
+     */
+    public getTasks(): Array<Task> {
+        return this.things.filter(thing => thing instanceof Task);
+    }
+
+    /**
+     * Returns the list of Events.
+     * @returns The list of Events.
+     */
+    public getEvents(): Array<Event> {
+        return this.things.filter(thing => thing instanceof Event);
+    }
+
+    /**
+     * Searches for a Thing by its ID.
+     * @param id - The ID of the Thing to search for.
+     * @returns The Thing with the given ID, or undefined if not found.
+     */
+    public getThingById(id: number): Thing | undefined {
+        return this.things.find(thing => thing.id === id);
     }
 
 }
