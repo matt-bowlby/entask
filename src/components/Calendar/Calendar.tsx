9e405b72ece9678787d94e8cdc008cf0e2e61877
JD Giovannetti <jonathangiovannetti2@gmail.com>
2025-03-17 09:27:15 -0700

    Change TodoList and Calendar width and growth settings

--- src/components/Calendar/Calendar.tsx
+++ src/components/Calendar/Calendar.tsx
@@ -3,8 +3,8 @@ import CalendarEvents from "@components/Calendar/CalendarEvents";
 
 export default function Calendar() {
     return (
-        <section className="bg-gray-200 grow-4 m-4">
-            <CalendarHeader></CalendarHeader>
+        <section className="bg-gray-200 grow m-4">
+            {/* <CalendarHeader></CalendarHeader> */}
             <CalendarEvents></CalendarEvents>
         </section>
     );
--- src/components/TodoList/TodoList.tsx
+++ src/components/TodoList/TodoList.tsx
@@ -2,7 +2,7 @@ import Task from "@/components/TodoList/Task";
 // import TodoEvent from "@/components/TodoList/TodoEvent";
 export default function TodoList() {
     return (
-        <section className="bg-[#F0F0F0] grow flex flex-col rounded-3xl m-4 overflow-hidden gap-1">
+        <section className="bg-[#F0F0F0] w-[400px] flex flex-col rounded-3xl m-4 overflow-hidden gap-1">
             <div className="todo-header flex flex-row justify-between items-center h-16 p-8 rounded-2xl bg-white drop-shadow-md">
                 <h1 className="font-bold text-2xl">To-Do</h1>
                 <h2 className="text-lg">
