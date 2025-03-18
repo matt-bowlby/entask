65ec1913869ddbcb8c0d6145c660e1bdb883b9b1
JD Giovannetti <jonathangiovannetti2@gmail.com>
2025-03-18 12:32:48 -0700

    Fix height and scrolling issues due to vh being too tall

--- src/components/TodoList/TodoList.tsx
+++ src/components/TodoList/TodoList.tsx
@@ -2,17 +2,28 @@ import Task from "@/components/TodoList/Task";
 // import TodoEvent from "@/components/TodoList/TodoEvent";
 export default function TodoList() {
     return (
-        <section className="bg-[#F0F0F0] w-[400px] flex flex-col rounded-3xl m-4 overflow-hidden gap-1">
-            <div className="todo-header flex flex-row justify-between items-center h-16 p-8 rounded-2xl bg-white drop-shadow-md">
-                <h1 className="font-bold text-2xl">To-Do</h1>
-                <h2 className="text-lg">
-                    <span className="font-bold text-2xl">13 </span>
-                    Thu
-                </h2>
+        <section className="bg-dark p-4 h-[95vh]">
+            <div className="relative bg-[#f0f0f0] w-[400px] h-full flex flex-col rounded-3xl overflow-auto gap-1">
+                <div className="sticky top-0 z-10 todo-header flex flex-row justify-between items-center h-16 p-8 rounded-2xl bg-white drop-shadow-md">
+                    <h1 className="font-bold text-2xl">To-Do</h1>
+                    <h2 className="text-lg">
+                        <span className="font-bold text-2xl">13 </span>
+                        Thu
+                    </h2>
+                </div>
+                <div className="flex-col overflow-auto pb-2">
+                    <Task></Task>
+                    <Task></Task>
+                    <Task></Task>
+                    <Task></Task>
+                    <Task></Task>
+                    <Task></Task>
+                    <Task></Task>
+                    <Task></Task>
+                    <Task></Task>
+                    <Task></Task>
+                </div>
             </div>
-            <Task></Task>
-            <Task></Task>
-            <Task></Task>
         </section>
     );
 }
