800a30146e195e4ef6b285c3ed3dce9e9ed5b1d4
JD Giovannetti <jonathangiovannetti2@gmail.com>
2025-03-18 12:45:02 -0700

    Remove CalendarHeader component

--- src/components/Calendar/Calendar.tsx
+++ src/components/Calendar/Calendar.tsx
@@ -1,10 +1,8 @@
-import CalendarHeader from "@components/Calendar/CalendarHeader";
 import CalendarEvents from "@components/Calendar/CalendarEvents";
 
 export default function Calendar() {
     return (
         <section className="bg-gray-200 grow m-4">
-            {/* <CalendarHeader></CalendarHeader> */}
             <CalendarEvents></CalendarEvents>
         </section>
     );
