4af4d2245bff82423627e6c6761c69a9cca27a13
Matt Bowlby <mbowlby794@gmail.com>
2025-04-01 17:14:55 -0700

    Add Task Integration with UI

--- src/components/TodoList/Task.tsx
+++ src/components/TodoList/Task.tsx
@@ -1,18 +1,24 @@
 import { ChevronDown } from "lucide-react";
+import Thing, {Task, Event} from "../../classes/thing/Thing";
 
-export default function Task() {
+interface TaskProps {
+    task: Task;
+}
+
+export default function TodoTaskComponent( { task }: TaskProps) {
     return (
-        <div className="bg-white drop-shadow-md max-w-full my-1 mx-4 p-2 h-28 flex rounded-3xl gap-4 relative">
+        <div className="bg-white drop-shadow-md max-w-full p-2 h-28 flex rounded-md gap-4 relative">
             <div className="tag-bar w-2 m-2 rounded-full overflow-hidden flex flex-col">
                 <div className="w-full h-full bg-red-200"></div>
                 <div className="w-full h-full bg-blue-200"></div>
             </div>
-            <div>
+            <div className="overflow-clip flex flex-col">
                 <h3 className="font-bold text-xl">
-                    Test Task with a Long Name
+                    {task.name}
                 </h3>
-                <p className="text-gray-600">2 hours</p>
-                <p className="text-gray-600">Due in 10 hours at 11:59pm</p>
+                <p className="text-gray-600">{(task.getDuration() / 1000 / 60 / 60) + " hrs"}</p>
+                <p className="text-gray-600">{"Due in " + (task.getTimeUntilDue() / 1000 / 60 / 60) + " hrs"}</p>
+                <p className="text-gray-600">{task.description}</p>
             </div>
             <ChevronDown className="absolute right-4 bottom-2 cursor-pointer" />
         </div>
