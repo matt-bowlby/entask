f928450210dc4cde31a41935d99193c0c16f009e
JD Giovannetti <jonathangiovannetti2@gmail.com>
2025-04-06 15:28:54 -0700

    Add basic testing buttons for removing and adding from global state, remove later

--- src/App.tsx
+++ src/App.tsx
@@ -1,43 +1,62 @@
 import TodoList from "@/components/TodoList/TodoListComponent";
 import CalendarView from "@/components/Calendar/CalendarComponent";
 import TitleBar from "./components/layout/TitleBarComponent";
-import Calendar from "./classes/calendar/Calendar";
 import { Task, Event } from "./classes/thing/Thing";
 import Tag from "./classes/tag/Tag";
+import useCalendarStore from "./store/calendarStore";
 
 function App() {
-    let calendar: Calendar = new Calendar("Calendar test");
-    let tag1 = new Tag("Tag 1", "This is a test tag", "7FB4DA");
-    let tag2 = new Tag("Tag 2", "This is another test tag", "5ED27D");
-    let test: Date = new Date();
-    test.setHours(3, 0, 0, 0);
-    calendar.addThing(
-        new Event(
-            "Task 1 Task 1 Task 1 Task 1",
-            1000 * 60 * 60,
-            test.getTime(),
-            "This is a test task",
-            [tag1, tag2]
-        )
-    );
-    calendar.addThing(
-        new Task(
-            "Task 2",
+    const calendarStore = useCalendarStore();
+
+    const handleAddThing = () => {
+        const tag = new Tag("Test Task", "Test Task Description", "7FB4DA");
+        const testDate = new Date();
+        testDate.setHours(3, 0, 0, 0);
+
+        const newThing = new Task(
+            "Test Task 1",
             1000 * 60 * 60 * 2,
             Date.now() + 1000 * 60 * 60 * 3,
-            undefined,
-            "",
-            [tag1],
+            testDate.getTime(),
+            "Test task description lololooololoo",
+            [tag],
             false
-        )
-    );
+        );
+
+        calendarStore.addThing(newThing);
+    };
+
+    const handleRemoveThing = () => {
+        const things = calendarStore.getAllThingsBetween(
+            0,
+            Date.now() + 1000 * 60 * 60 * 24
+        );
+        if (things.length > 0) {
+            calendarStore.removeThing(things[0]);
+        }
+    };
+
     return (
         <>
             <TitleBar />
+            <div className="flex gap-2 p-2 bg-dark">
+                <button
+                    onClick={handleAddThing}
+                    className="px-4 py-2 bg-blue-500 text-white rounded"
+                >
+                    Add Test Thing
+                </button>
+                <button
+                    onClick={handleRemoveThing}
+                    className="px-4 py-2 bg-red-500 text-white rounded"
+                >
+                    Remove First Thing
+                </button>
+            </div>
             <main className="flex h-[calc(100vh-var(--title-bar-height))] bg-dark p-2 gap-2 overflow-hidden">
-                <TodoList calendar={calendar} />
+                <TodoList calendar={calendarStore.calendar} />
                 <CalendarView
-                    calendar={calendar}
+                    calendar={calendarStore.calendar}
                     numDaysInView={5}
                     dayOffset={0}
                 />
