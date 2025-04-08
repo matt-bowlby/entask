1ee7477a72317f2e44e6981c03fafceed4c38b52
Matt Bowlby <mbowlby794@gmail.com>
2025-04-08 00:50:13 -0700

    Undo fix of timeString

--- src/utils/timeString.ts
+++ src/utils/timeString.ts
@@ -1,10 +1,10 @@
 export function msDurationToString(ms: number, abbreviate: boolean = false): string {
-	const years: number = Math.round(Math.abs(ms) / (1000 * 60 * 60 * 24 * 365));
-	const months: number = Math.round((Math.abs(ms) % (1000 * 60 * 60 * 24 * 365)) / (1000 * 60 * 60 * 24 * 30));
-	const weeks: number = Math.round((Math.abs(ms) % (1000 * 60 * 60 * 24 * 30)) / (1000 * 60 * 60 * 24 * 7));
-	const days: number = Math.round((Math.abs(ms) % (1000 * 60 * 60 * 24 * 7)) / (1000 * 60 * 60 * 24));
-	const hours: number = Math.round((Math.abs(ms) % (1000 * 60 * 60 * 24)) / (1000 * 60 * 60));
-	const minutes: number = Math.round((Math.abs(ms) % (1000 * 60 * 60)) / (1000 * 60));
+	const years: number = Math.floor(Math.abs(ms) / (1000 * 60 * 60 * 24 * 365));
+	const months: number = Math.floor((Math.abs(ms) % (1000 * 60 * 60 * 24 * 365)) / (1000 * 60 * 60 * 24 * 30));
+	const weeks: number = Math.floor((Math.abs(ms) % (1000 * 60 * 60 * 24 * 30)) / (1000 * 60 * 60 * 24 * 7));
+	const days: number = Math.floor((Math.abs(ms) % (1000 * 60 * 60 * 24 * 7)) / (1000 * 60 * 60 * 24));
+	const hours: number = Math.floor((Math.abs(ms) % (1000 * 60 * 60 * 24)) / (1000 * 60 * 60));
+	const minutes: number = Math.floor((Math.abs(ms) % (1000 * 60 * 60)) / (1000 * 60));
 
 	if (years > 0) return `${years}${abbreviate ? "y" : ` year${ years != 1 ? 's' : ''}`}`;
 	if (months > 0) return `${months}${abbreviate ? "mo" : ` month${ months != 1 ? 's' : ''}`}`;
