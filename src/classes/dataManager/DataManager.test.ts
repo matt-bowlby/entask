320eec8123ec69e644912c83f4e58e2da354fdb1
JoshuaKidwell <railsky10@gmail.com>
2025-04-10 14:51:17 -0700

    test task

--- src/classes/dataManager/DataManager.test.ts
+++ src/classes/dataManager/DataManager.test.ts
@@ -1,7 +1,7 @@
-//import { Event, Task } from "../thing/Thing";
+import { Task } from "../thing/Thing";
 //import Thing from "../thing/Thing";
 import DataManager from "./DataManager";
-import Calendar from "../calendar/Calendar";
+//import Calendar from "../calendar/Calendar";
 //import Thing from "../thing/Thing";
 
 describe('DataManager', () =>  {
@@ -26,8 +26,13 @@ describe('DataManager', () =>  {
     //     console.log(thing_list[0].getTags()[0].getName());
     // })
 
-    test('LoadDatabase', () => {
-        let calendar: Calendar = DataManager.loadDatabase("newCal");
-        console.log(calendar.getName());
+    // test('LoadDatabase', () => {
+    //     let calendar: Calendar = DataManager.loadDatabase("newCal");
+    //     console.log(calendar.getName());
+    // })
+
+    test('savestuff', () => {
+        const myTask = new Task("myTask", 2 * 1000 * 60 * 60, Date.now());
+        DataManager.saveTask(myTask);
     })
 });
