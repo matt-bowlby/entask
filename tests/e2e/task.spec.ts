3cf89a9fe1d21ed12192e0a68507254382c5d1e4
JD Giovannetti <jonathangiovannetti2@gmail.com>
2025-04-25 21:52:08 -0700

    Add screenshots during task testing

--- tests/e2e/task.spec.ts
+++ tests/e2e/task.spec.ts
@@ -1,21 +1,31 @@
 import { test, expect, _electron as electron } from "@playwright/test";
 import eph from 'electron-playwright-helpers';
 
+// empty the calendar using save-calendar ipc before test suite
+test.beforeAll(async () => {
+  const electronApp = await electron.launch({ args: ["dist-electron/main.js"] });
+  await eph.ipcMainInvokeHandler(electronApp, "save-calendar", "Calendar");
+  await electronApp.close();
+})
 
 test("creating a task", async () => {
   const electronApp = await electron.launch({ args: ["dist-electron/main.js"] });
   const window = await electronApp.firstWindow();
 
   await window.click("#create-new-btn");
+  await window.screenshot({ path: "test-results/task/01-open-new-task-dialog.png" });
   await window.fill("#name", "Playwright Test Task");
   await window.fill("#description", "Created by Playwright system test");
+  await window.screenshot({ path: "test-results/task/02-filled-task-fields.png" });
 
   const createBtn = window.getByTestId("create-btn");
   await expect(createBtn).toBeVisible();
   await expect(createBtn).toBeEnabled();
   await createBtn.click({ force: true });
+  await window.screenshot({ path: "test-results/task/03-task-created.png" });
 
   await expect(window.locator("text=Playwright Test Task")).toBeVisible();
+  await window.screenshot({ path: "test-results/task/04-task-visible-in-ui.png" });
 
   // wait for debounce
   await window.waitForTimeout(1000);
@@ -40,16 +50,20 @@ test("editing a task", async () => {
   const editTask = await window.locator("text=Playwright Test Task");
   await expect(editTask).toBeVisible();
   await editTask.click();
+  await window.screenshot({ path: "test-results/task/05-open-edit-dialog.png" });
 
   await window.fill("#name", "Playwright Test Task (edited)");
   await window.fill("#description", "Edited by Playwright system test");
+  await window.screenshot({ path: "test-results/task/06-edited-task-fields.png" });
 
   const saveBtn = await window.getByTestId("save-btn");
   await expect(saveBtn).toBeVisible();
   await expect(saveBtn).toBeEnabled();
   await saveBtn.click({ force: true });
+  await window.screenshot({ path: "test-results/task/07-after-save-edit.png" });
 
   await expect(window.locator("text=Playwright Test Task")).toBeVisible();
+  await window.screenshot({ path: "test-results/task/08-edited-task-visible-in-ui.png" });
 
   // wait for debounce
   await window.waitForTimeout(1000);
@@ -74,13 +88,16 @@ test("deleting a task", async () => {
   const editTask = await window.locator("text=Playwright Test Task (edited)");
   await expect(editTask).toBeVisible();
   await editTask.click();
+  await window.screenshot({ path: "test-results/task/09-open-edit-dialog-for-delete.png" });
 
   const deleteBtn = await window.getByTestId("delete-btn");
   await expect(deleteBtn).toBeVisible();
   await expect(deleteBtn).toBeEnabled();
   await deleteBtn.click({ force: true });
+  await window.screenshot({ path: "test-results/task/10-after-delete.png" });
 
   await expect(window.locator("text=Playwright Test Task (edited)")).not.toBeVisible();
+  await window.screenshot({ path: "test-results/task/11-task-not-visible-after-delete.png" });
 
   // wait for debounce
   await window.waitForTimeout(1000);
